02bced8c4d1b04c39484c1780f87293a72d7357c
Pedro Ananias <pedro13.2010@gmail.com>
2022-09-14 01:15:46 -0300

    Commit Inicial

---
+++ README.txt
@@ -0,0 +1,25 @@
+=> PT-BR
+
+=================   MARIO JUMP   ===============
+Olá, esse projeto foi meu primeiro game utilizando HTML, CSS e JavaScript.
+Precione qualquer tecla para fazer o Mario pular os canos do cenário sem toca-los.
+Quando Mario morrer precione F5 para reiniciar o jogo.
+
+
+========== ATUALIZAÇÕES DO MARIO JUMP ==========
+1.AS NUVENS PARAM DE SE MOVER QUANDO MARIO MORRE;
+
+2.QUANDO MARIO MORRE ELE FAZ ANIMAÇÃO SEMELHANTE DO SUPER NINTENDO E SAI DO CENÁRIO;
+
+=> EN
+
+=================   MARIO JUMP   ===============
+Hello, this project was my first game using HTML, CSS and JavaScript.
+Press any key to make Mario jump over the pipes on the stage without touching them.
+When Mario dies, press F5 to restart the game.
+
+
+============   MARIO JUMP UPDATES   ============
+1.THE CLOUDS STOP MOVING WHEN MARIO DIES;
+
+2.WHEN MARIO DIES HE MAKES ANIMATION SIMILAR TO SUPER NINTENDO AND LEAVES THE SCENE;
---
+++ script.js
@@ -0,0 +1,40 @@
+const mario = document.querySelector('.mario')
+const pipe = document.querySelector('.pipe')
+const clouds1 = document.querySelector('.clouds1')
+const clouds2 = document.querySelector('.clouds2')
+
+const jump = () => {
+    mario.classList.add('jump')
+    setTimeout(() => {
+        mario.classList.remove('jump')
+    }, 700)
+    
+}
+
+const loop = setInterval(() => {
+    const pipePosition = pipe.offsetLeft
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '')
+    const clouds1Position = clouds1.offsetLeft
+    const clouds2Position = clouds2.offsetLeft
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+        pipe.style.animation = 'none'
+        pipe.style.left = `${pipePosition}px`
+        mario.style.animation = 'none'
+        mario.style.bottom = `${marioPosition}px`
+        mario.src = '/images/game-over.png'
+        mario.style.width = '75px'
+        mario.style.marginLeft = '50px'
+        mario.style.animation = 'death 1s'
+        setTimeout(() => {
+            mario.style.top = '100%'
+        }, 1000) 
+        clouds1.style.animation = 'none'
+        clouds2.style.animation = 'none'
+        clouds1.style.left = `${clouds1Position}px`
+        clouds2.style.left = `${clouds2Position}px`
+        clouds2.style.top = '50px'
+        clearInterval(loop)
+    }
+}, 10)
+
+document.addEventListener('keydown', jump)
